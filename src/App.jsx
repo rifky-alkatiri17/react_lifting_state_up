e235d1ce681b1122b519e456aac9ae15a1ad8e92
rifky-alkatiri17 <rifkyalkatiri17@gmail.com>
2025-12-09 10:16:49 +0800

    update folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,37 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+export default function Parent() {
   const [count, setCount] = useState(0)
+
+  function handleClick(v){
+    // alert(`halo from parent, child:${v}`)
+    console.log(`halo from parent, child:${v}`)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h2>LIFTING STATE UP</h2>    
+      <div className="parent">      
+        <Child handleClick={handleClick}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+
+function Child({handleClick}){
+  return(
+    <div className="child">
+      <button id="btn" onClick={()=>handleClick(1)}> 1 </button>
+      <button id="btn" onClick={()=>handleClick(2)}> 2 </button>
+      <button id="btn" onClick={()=>handleClick(3)}> 3 </button>
+
+      <button id="btn" onClick={()=>handleClick(4)}> 4 </button>
+      <button id="btn" onClick={()=>handleClick(5)}> 5 </button>
+      <button id="btn" onClick={()=>handleClick(6)}> 6 </button>
+    </div>
+  )
+}
+
+
